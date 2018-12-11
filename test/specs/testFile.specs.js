a5dad2836e483ae764c0822d1b0a3d31ce7f2293
satyam soni <satyamsoni@hotmail.co.uk>
2018-12-12 00:22:03 +0530

    Added test case using chai, mocha and coverage using istanbull

---
+++ test/specs/testFile.specs.js
@@ -0,0 +1,7 @@
+import { assert } from 'chai';
+
+describe('Check for truth', () => {
+  it('check if sum of 2 and 3 is 5', () => {
+    assert.equal(2 + 3, 5, 'sum should be equal to 5');
+  });
+});
